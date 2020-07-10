726a0bb23d50719bbffe6cb2fb5f7102c9f0a95f
Matthew P. Kerle <matt.kerle@smartrent.com>
2020-07-09 21:26:22 -0700

    Add Missing Files

---
+++ lib/log.ex
@@ -0,0 +1,18 @@
+defmodule Log do
+  @moduledoc """
+  Documentation for Log.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Log.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
