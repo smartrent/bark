100db5d0b1ffd36b0b8cdef5f45179688f472f3d
Ryan Raub <>
2020-09-04 13:35:06 -0700

    adding line numbers, tests, and some refactoring

--- test/log_test.exs
+++ test/log_test.exs
@@ -1,8 +1,28 @@
 defmodule LogTest do
-  use ExUnit.Case
-  doctest Log
+  use ExUnit.Case, async: true
+  doctest Bark
 
-  test "greets the world" do
-    assert Log.hello() == :world
+  describe "info" do
+    test "greets the world" do
+      assert Bark.info(__ENV__, speak: "woof", number: 1, map: %{something: "neet"}, tuple: {:tuple, :ok} ) == :ok
+    end
+  end
+
+  describe "warn" do
+    test "greets the world" do
+      assert Bark.warn(__ENV__, speak: "woof", number: 1, map: %{something: "neet"}, tuple: {:tuple, :ok} ) == :ok
+    end
+  end
+
+  describe "error" do
+    test "greets the world" do
+      assert Bark.error(__ENV__, speak: "woof", number: 1, map: %{something: "neet"}, tuple: {:tuple, :ok} ) == :ok
+    end
+  end
+
+  describe "debug" do
+    test "greets the world" do
+      assert Bark.debug(__ENV__, speak: "woof", number: 1, map: %{something: "neet"}, tuple: {:tuple, :ok} ) == :ok
+    end
   end
 end
